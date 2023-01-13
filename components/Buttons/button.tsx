9599ca618194941ed6400232195dba9b5c289015
femidamilola <45465232+femidamilola@users.noreply.github.com>
2023-01-13 20:26:39 +0100

    Cypress and Tailwind

--- components/Buttons/button.tsx
+++ components/Buttons/button.tsx
@@ -3,5 +3,15 @@ interface ButtonProps {
   onClick?: () => void
 }
 export const Button = ({text, onClick}: ButtonProps) => <>
-  <button onClick={onClick}>{text}</button>
+  <button className={'bg-blue-500 text-white px-4.5 py-3'} onClick={onClick}>{text}</button>
+</>
+
+interface IButtonProps extends ButtonProps {
+  icon: JSX.Element
+}
+export const IButton = ({text, onClick, icon}: IButtonProps) => <>
+  <button className={'flex flex-row gap-1 bg-blue-500 text-white px-4.5 py-3'} onClick={onClick}>
+    {icon}
+    <span>{text}</span>
+  </button>
 </>
--- cypress/support/component.js
+++ cypress/support/component.js
@@ -1,3 +1,4 @@
+import '../../styles/globals.css'
 // ***********************************************************
 // This example support/component.js is processed and
 // loaded automatically before your test files.
